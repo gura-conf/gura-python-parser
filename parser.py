df063a2b7564ab55e8a0316474a07ac45cc35f20
Genarito <genarocamele@hotmail.com>
2021-03-22 13:04:55 -0300

    + Added types to ParserError init method

--- parser.py
+++ parser.py
@@ -2,13 +2,14 @@ from typing import Dict, List, Optional, Any
 
 
 class ParseError(Exception):
-    def __init__(self, pos, msg, *args):
+    def __init__(self, pos: Optional[int], msg: str, *args):
         self.pos = pos
         self.msg = msg
         self.args = args
 
     def __str__(self):
         return '%s at position %s' % (self.msg % self.args, self.pos)
+
 
 class Parser:
     def __init__(self):
