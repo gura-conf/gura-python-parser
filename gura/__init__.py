44678fc0bd2b61dd5f9e48f05f80ab1ec9c8c70d
Genarito <genarocamele@hotmail.com>
2021-11-10 16:42:30 -0300

    Fixed import

--- gura/__init__.py
+++ gura/__init__.py
@@ -1,11 +1,12 @@
 from gura.GuraParser import GuraParser, InvalidIndentationError, DuplicatedVariableError, DuplicatedKeyError, \
     VariableNotDefinedError, DuplicatedImportError, loads, dumps
-from gura.Parser import ParseError
+from gura.Parser import ParseError, GuraError
 
 __version__ = "1.4.3"
 
 loads = loads
 dumps = dumps
+GuraError = GuraError
 ParseError = ParseError
 InvalidIndentationError = InvalidIndentationError
 DuplicatedVariableError = DuplicatedVariableError
