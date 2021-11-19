fa025e66df0cb9297fa9892508bb8387ba72177d
Genarito <genarocamele@hotmail.com>
2021-11-18 21:03:23 -0300

    Added missing quotes in Exception messages

--- gura/Parser.py
+++ gura/Parser.py
@@ -36,7 +36,7 @@ class Parser:
             raise ParseError(
                 self.pos + 1,
                 self.line,
-                'Expected end of string but got %s',
+                'Expected end of string but got "%s"',
                 self.text[self.pos + 1]
             )
 
@@ -101,7 +101,7 @@ class Parser:
         raise ParseError(
             self.pos + 1,
             self.line,
-            'Expected [%s] but got %s' % (chars, next_char)
+            'Expected [%s] but got "%s"' % (chars, next_char)
         )
 
     def keyword(self, *keywords: str):
